7efe009e31d5141a6517c907c057af54d9387caf
f00barin <pranava@cs.upc.edu>
2015-11-02 16:54:00 +0100

    d

--- glm-parser/src/learn/perceptron.py
+++ glm-parser/src/learn/perceptron.py
@@ -27,6 +27,87 @@ class PerceptronLearner():
             max_iter = self.max_iter
 
         logging.debug("Starting sequantial train...")
+
+
+        for i in range(max_iter):
+            logging.debug("Iteration: %d" % i)
+            logging.debug("Data size: %d" % len(data_pool.data_list))
+            headlist = [w.strip().split()[0] for w in
+                        open('penn-wsj-deps/reps/07/ptb.train.original.heads.reps')]
+            modlist = [w.strip().split()[0] for w in
+                       open('penn-wsj-deps/reps/07/ptb.train.original.mods.reps')]
+            labels = ['AMOD','DEP','NMOD','OBJ','P','PMOD','PRD','SBAR','SUB','VC','VMOD']
+            s = shelve.open('lbfeatures.db', flag='c', writeback=True)
+            for label in labels:
+                hmmat = np.load('penn-wsj-deps/reps/bt.'+label+'.npy')
+                while data_pool.has_next_data():
+                    self.sentence += 1
+                    sentno = str(self.sentence)
+                    data_instance = data_pool.get_next_data()
+                    print label, self.sentence
+                    for h, m in permutations(data_instance.word_list,2):
+                        if h in headlist and m in modlist:
+                            head = headlist.index(h)
+                            mod = modlist.index(m)
+                            if label in s:
+                                if not (h,m) in s[label]:
+                                    s[label][(h,m)] = (str((5, 0, h, m, label)),
+                                                    hmmat[head, mod])
+                            else:
+                                s[sentno] = {(h,m):(str((5, 0, h, m, label)),
+                                                    hmmat[head, mod])}
+                    s.sync()
+            s.sync()
+            s.close()
+
+
+    def update_weight(self, current_global_vector, gold_global_vector):
+        # otherwise, the gold_global_vector will change because of the change in weights
+        for key in gold_global_vector.keys():
+            self.w_vector.data_dict[key].iadd(gold_global_vector[key].feature_dict)
+
+#        print 'Gold ', gold_global_vector
+#        print 'Current ', current_global_vector
+
+        for key in current_global_vector.iterkeys():
+            self.w_vector.data_dict[key].iaddc(current_global_vector[key].feature_dict, -1)
+
+        return
+#            db = sqlite3.connect('validation_sql.db')
+#            cur = db.cursor()
+#            cur.execute('''CREATE TABLE features (sentno integer,  head
+##                        integer, mod integer, feats text)''')
+##            db.commit()
+#            db.close()
+#                    print h,m
+#                    cur = db.cursor()
+#                    cur.execute("INSERT INTO features VALUES (?,?,?,?)",
+#                               (self.sentence, h, m, '###join###'.join(data_instance.get_local_vector(h,m)[0])))
+#                    db.commit()
+#
+#                for h,m in hmlist:
+#                    print 'this is %s, %s' % (h,m), data_instance.get_local_vector(h,m)
+##                    cur.execute("INSERT INTO features VALUES (?,?,?,?)",
+##                               (self.sentence, h, m,
+##                                '###join###'.join(data_instance.get_local_vector(h,m,
+##                                                                                 self.sentence)[0])))
+##                db.commit()
+#
+
+#                gold_global_vector = data_instance.gold_global_vector
+##                print gold_global_vector.keys()
+#                current_global_vector = f_argmax(data_instance, self.sentence)
+##                print current_global_vector.keys()
+#                self.update_weight(current_global_vector, gold_global_vector)
+#
+#            data_pool.reset_index()
+#            if d_filename is not None:
+#                if i % dump_freq == 0 or i == max_iter - 1:
+#                    self.w_vector.dump(d_filename + "_Iter_%d.db"%i)
+#
+
+
+
 #
 #        s = shelve.open('biltraining.db', flag='c', writeback=True)
 #        headreps = {}
@@ -81,80 +162,6 @@ class PerceptronLearner():
 #                    s.sync()
 #        s.close()
 #
-
-        for i in range(max_iter):
-            logging.debug("Iteration: %d" % i)
-            logging.debug("Data size: %d" % len(data_pool.data_list))
-
-            s = shelve.open('learndataset.db', flag='c', writeback=True)
-#            db = sqlite3.connect('validation_sql.db')
-#            cur = db.cursor()
-#            cur.execute('''CREATE TABLE features (sentno integer,  head
-##                        integer, mod integer, feats text)''')
-##            db.commit()
-            while data_pool.has_next_data():
-                self.sentence += 1
-#                cur = db.cursor()
-                sentno = str(self.sentence)
-                data_instance = data_pool.get_next_data()
-                print self.sentence
-                for h, m in permutations(range(len(data_instance.word_list)),2):
-##                    cur.execute("INSERT INTO features VALUES (?,?,?,?)",
-##                               (self.sentence, h, m,
-##                                '###join###'.join(data_instance.get_local_vector(h,m,
-##                                                                                 self.sentence)[0])))
-##                db.commit()
-#
-                    if sentno in s:
-                        s[sentno][(h,m)] = data_instance.get_local_vector(h,m,
-                                                                          self.sentence)[0]
-                    else:
-                        s[sentno] = {(h,m):
-                                     data_instance.get_local_vector(h,m,
-                                                                    self.sentence)[0]}
-                s.sync()
-            s.close()
-#            db.close()
-#                    print h,m
-#                    cur = db.cursor()
-#                    cur.execute("INSERT INTO features VALUES (?,?,?,?)",
-#                               (self.sentence, h, m, '###join###'.join(data_instance.get_local_vector(h,m)[0])))
-#                    db.commit()
-#
-#                for h,m in hmlist:
-#                    print 'this is %s, %s' % (h,m), data_instance.get_local_vector(h,m)
-
-
-#                gold_global_vector = data_instance.gold_global_vector
-##                print gold_global_vector.keys()
-#                current_global_vector = f_argmax(data_instance, self.sentence)
-##                print current_global_vector.keys()
-#                self.update_weight(current_global_vector, gold_global_vector)
-#
-#            data_pool.reset_index()
-#            if d_filename is not None:
-#                if i % dump_freq == 0 or i == max_iter - 1:
-#                    self.w_vector.dump(d_filename + "_Iter_%d.db"%i)
-#
-
-    def update_weight(self, current_global_vector, gold_global_vector):
-        # otherwise, the gold_global_vector will change because of the change in weights
-        for key in gold_global_vector.keys():
-            self.w_vector.data_dict[key].iadd(gold_global_vector[key].feature_dict)
-
-#        print 'Gold ', gold_global_vector
-#        print 'Current ', current_global_vector
-
-        for key in current_global_vector.iterkeys():
-            self.w_vector.data_dict[key].iaddc(current_global_vector[key].feature_dict, -1)
-
-        return
-
-
-
-
-
-
 
 
 
